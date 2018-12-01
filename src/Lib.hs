6b50fc43151a6ceca8087b724585f72a777e6c13
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2018-12-01 15:52:31 -0500

    Moved printing to runOnFile

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,11 +5,11 @@ import System.IO
 someFunc :: IO ()
 someFunc = return ()
 
-runOnFile :: (String -> IO a) -> String -> IO ()
+runOnFile :: Show a => (String -> a) -> String -> IO ()
 runOnFile f fname =
     openFile fname ReadMode >>= \handle ->
     hGetContents handle >>= \contents ->
-    f contents *>
+    print (f contents) *>
     hClose handle
 
 toNum :: String -> [Integer]
@@ -18,10 +18,10 @@ toNum = fmap f . words
           f ('-':xs) = -(read xs)
 
 day1p1 :: IO ()
-day1p1 = runOnFile (print . sum . toNum) "day1.txt"
+day1p1 = runOnFile (sum . toNum) "day1.txt"
 
 day1p2 :: IO ()
-day1p2 = runOnFile (print . fst . head . filter seenFreq . buildFreqs . cycle . toNum) "day1.txt"
+day1p2 = runOnFile (fst . head . filter seenFreq . buildFreqs . cycle . toNum) "day1.txt"
     where buildFreqs xs = let freqs = drop 1 $ scanl (+) 0 xs
                           in zip freqs $ scanl (flip (:)) [] freqs
           seenFreq (freq, seen) = freq `elem` seen
