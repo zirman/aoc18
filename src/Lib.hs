3cfbf015cff952f7bdbddadb1a2fa5f7d1924543
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2018-12-03 13:14:50 -0500

    day3

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,9 @@ module Lib where
 import qualified System.IO as IO
 import qualified Data.List as List
 import qualified Data.Maybe as Maybe
+import qualified Data.Char as Char
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 someFunc :: IO ()
 someFunc = return ()
@@ -36,7 +39,7 @@ day2p1 = runOnFile day2 "day2.txt" where
     countLetters :: String -> [Int]
     countLetters = fmap head . List.group . List.sort . countRepeats
     countRepeats :: Ord a => Eq a => [a] -> [Int]
-    countRepeats = filter (/=1) . fmap length . List.group . List.sort
+    countRepeats = filter (/= 1) . fmap length . List.group . List.sort
 
 day2p2 :: IO ()
 day2p2 = runOnFile day2 "day2.txt" where
@@ -47,10 +50,34 @@ day2p2 = runOnFile day2 "day2.txt" where
         List.groupBy subStrCmp . List.sort .
         fmap subStrs . lines
     subStrs :: [a] -> [[a]]
-    subStrs = g [] where
-        g ps [] = []
-        g ps (x : ts) = (ps ++ ts) : g (ps ++ [x]) ts
+    subStrs = f [] where
+        f ps [] = []
+        f ps (x : ts) = (ps ++ ts) : f (ps ++ [x]) ts
     subStrCmp :: Eq a => [[a]] -> [[a]] -> Bool
     subStrCmp xs ys = any (`elem` ys) xs
     findInLists :: Foldable t => Eq a => t a -> t a -> [a]
     findInLists xs = concatMap (\y -> Maybe.maybeToList (List.find (== y) xs))
+
+day3p1 :: IO ()
+day3p1 = runOnFile day3 "day3.txt" where
+    day3 = length . filter (\xs -> length xs > 1) . List.group . List.sort . concatMap (snd . readClaim . words) . lines
+
+readClaim :: [String] -> (String, [(Int, Int)])
+readClaim ['#':cNum, "@", offset, size] =
+    let (Just i) = List.elemIndex ',' offset
+    in let (Just j) = List.elemIndex 'x' size
+    in let (x, ',':y) = splitAt i offset
+    in let y' = read (filter Char.isDigit y) :: Int
+    in let x' = read x :: Int
+    in let (w, 'x':h) = splitAt j size
+    in let w' = read w :: Int
+    in let h' = read h :: Int
+    in (cNum, [(a, b) | a <- [x'..(x' + w' - 1)], b <- [y'..y' + h' - 1]])
+
+day3p2 :: IO ()
+day3p2 = runOnFile (day3 . claims) "day3.txt" where
+    claims :: String -> [(String, [(Int, Int)])]
+    claims = fmap (readClaim . words) . lines
+    day3 xs = fmap fst . filter (\(cNum, q) -> Set.null (Set.fromList q `Set.intersection` overlaping)) $ xs where
+        overlaping :: Set (Int, Int)
+        overlaping = Set.fromList (List.concat (filter (\xs -> length xs > 1) . List.group . List.sort . concatMap snd $ xs))
