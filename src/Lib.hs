f897fd404ffd79e4fa6c6c8d250b131ac78c58bc
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2018-12-05 13:37:24 -0500

    day 4

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,12 @@ import qualified Data.Maybe as Maybe
 import qualified Data.Char as Char
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Text.ParserCombinators.Parsec
+import Data.Functor
+import Data.Time.Calendar
+import Text.Parsec.Prim
+import Text.Parsec.Pos (SourcePos(..))
+-- import Data.Semigroup
 
 someFunc :: IO ()
 someFunc = return ()
@@ -81,3 +87,131 @@ day3p2 = runOnFile (day3 . claims) "day3.txt" where
     day3 xs = fmap fst . filter (\(cNum, q) -> Set.null (Set.fromList q `Set.intersection` overlaping)) $ xs where
         overlaping :: Set (Int, Int)
         overlaping = Set.fromList (List.concat (filter (\xs -> length xs > 1) . List.group . List.sort . concatMap snd $ xs))
+
+day4p1 :: IO ()
+day4p1 = runOnFile (toRanges . findMaxNap . addNaps . mergeNaps . groupByGuard . sortByGuard . parseShifts . sortByTimeStamp . parseLogFile) "day4.txt" where
+    parseLogFile = parse pLogFile "Parsing Log File"
+    sortByTimeStamp (Right xs) = List.sort xs
+    parseShifts = parse pShifts "Parsing Guard Shifts"
+    sortByGuard (Right xs) = List.sortBy (\a b -> compare (fst a) (fst b)) xs
+    groupByGuard = List.groupBy (\a b -> fst a == fst b)
+    mergeNaps = fmap (\xs -> (fst (head xs), concatMap snd xs))
+    addNaps = fmap (\(guard, naps) -> (guard, naps, sum . fmap napLength $ naps))
+    findMaxNap = List.maximumBy (\(_, _, a) (_, _, b) -> compare a b)
+    toRanges (guard, naps, _) = guard * (snd . head . List.maximumBy (\a b -> compare (length a) (length b)) . List.group . List.sort . concatMap rangeMinutes $ naps)
+
+day4p2 :: IO ()
+day4p2 = runOnFile (calcNum . mergeGuard . mergeNaps . groupByGuard . sortByGuard . parseShifts . sortByTimeStamp . parseLogFile) "day4.txt" where
+    parseLogFile = parse pLogFile "Parsing Log File"
+    sortByTimeStamp (Right xs) = List.sort xs
+    parseShifts = parse pShifts "Parsing Guard Shifts"
+    sortByGuard (Right xs) = List.sortBy (\a b -> compare (fst a) (fst b)) xs
+    groupByGuard = List.groupBy (\a b -> fst a == fst b)
+    mergeNaps = fmap (\xs -> (fst (head xs), concatMap rangeMinutes . concatMap snd $ xs))
+    mergeGuard = List.maximumBy (\(_, (b, _)) (_, (e, _)) -> compare b e) . fmap (\(guard, naps) -> (guard, foo . List.maximumBy (\a b -> compare (length a) (length b)) . List.group . List.sort $ naps)) . List.filter (\(_, naps) -> not (null naps))
+    calcNum (guard, (count, (hour, minute))) = guard * minute
+
+foo xs = (length xs, head xs)
+
+napLength :: (TimeStamp, TimeStamp) -> Integer
+napLength (start, end) = toJulianSecond end - toJulianSecond start
+
+rangeMinutes :: (TimeStamp, TimeStamp) -> [(Int, Int)]
+rangeMinutes (start, end) = fmap (f . toJulianTime) [toJulianSecond start..toJulianSecond end - 1] where
+    f (_, h, m) = (h, m)
+
+toJulianSecond :: TimeStamp -> Integer
+toJulianSecond TimeStamp { day=d, hour=h, minute=m } =
+    (24 * 60 * toModifiedJulianDay d) + 60 * fromIntegral h + fromIntegral m
+
+toJulianTime :: Integer -> (Day, Int, Int)
+toJulianTime j =
+    ( ModifiedJulianDay { toModifiedJulianDay = div j (24 * 60) }
+    , fromIntegral (div (mod j (24 * 60)) 60)
+    , fromIntegral (mod j 60)
+    )
+
+data TimeStamp = TimeStamp {
+    day :: Day,
+    hour :: Int,
+    minute :: Int
+} deriving (Eq, Ord, Show)
+
+data LogTok
+    = BeginShiftTok {
+        timeStamp :: TimeStamp,
+        idNum :: Int
+    }
+    | WakeUpTok { timeStamp :: TimeStamp }
+    | FallAsleepTok { timeStamp :: TimeStamp } deriving (Eq, Show)
+
+instance Ord LogTok where
+    compare a b = compare (getTimeStamp a) (getTimeStamp b)
+
+getTimeStamp :: LogTok -> TimeStamp
+getTimeStamp BeginShiftTok { timeStamp = timeStamp } = timeStamp
+getTimeStamp WakeUpTok { timeStamp = timeStamp } = timeStamp
+getTimeStamp FallAsleepTok { timeStamp = timeStamp } = timeStamp
+
+pLogLine :: GenParser Char st LogTok
+pLogLine =
+    pTimeStamp >>= \ts ->
+    char ' ' *>
+    (
+        (
+            string "Guard #" *> many1 digit >>= \i ->
+            string " begins shift\n" $> BeginShiftTok {
+                timeStamp = ts,
+                idNum = read i
+            }
+        ) <|>
+        string "falls asleep\n" $> FallAsleepTok { timeStamp = ts } <|>
+        string "wakes up\n" $> WakeUpTok { timeStamp = ts }
+    )
+
+pTimeStamp :: GenParser Char st TimeStamp
+pTimeStamp =
+    char '[' *> many1 digit >>= \y ->
+    char '-' *> many1 digit >>= \mo ->
+    char '-' *> many1 digit >>= \d ->
+    char ' ' *> many1 digit >>= \h ->
+    char ':' *> many1 digit >>= \mi ->
+    char ']' $> TimeStamp {
+        day = fromGregorian (read y) (read mo) (read d),
+        hour = read h,
+        minute = read mi
+    }
+
+pLogFile :: GenParser Char st [LogTok]
+pLogFile = many pLogLine <* eof
+
+satisfyT :: (Show t, Stream s m t) => (t -> Bool) -> ParsecT s u m t
+satisfyT p = tokenPrim show nextPos testTok where
+    updatePosTok pos c _cs = updatePosChar pos c
+    nextPos pos x xs = pos -- don't care about pos
+    testTok t        = if p t then Just t else Nothing
+    updatePosChar a = a
+
+pBeginShift :: GenParser LogTok st LogTok
+pBeginShift = satisfyT (\x -> case x of BeginShiftTok {} -> True; _ -> False)
+
+pFallAsleep :: GenParser LogTok st LogTok
+pFallAsleep = satisfyT (\x -> case x of FallAsleepTok {} -> True; _ -> False)
+
+pWakeUp :: GenParser LogTok st LogTok
+pWakeUp = satisfyT (\x -> case x of WakeUpTok {} -> True; _ -> False)
+
+pNap :: GenParser LogTok st (TimeStamp, TimeStamp)
+pNap =
+    pFallAsleep >>= \FallAsleepTok { timeStamp = startTime } ->
+    pWakeUp >>= \WakeUpTok { timeStamp = endTime } ->
+    return (startTime, endTime)
+
+pShift :: GenParser LogTok st (Int, [(TimeStamp, TimeStamp)])
+pShift =
+    pBeginShift >>= \BeginShiftTok { idNum = idNum } ->
+    many pNap >>= \naps ->
+    return (idNum, naps)
+
+pShifts :: GenParser LogTok st [(Int, [(TimeStamp, TimeStamp)])]
+pShifts = many pShift
